60d75df267c57b671f92b56ef8834a23f1088bdd
Raphael Borges <rah-borges@hotmail.com>
2021-02-15 18:34:14 -0300

    feat(login):form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('usuairioInput') UsuairioInput:ElementRef|undefined
+  @ViewChild('senhaInput') SenhaInput:ElementRef|undefined
+
+  usuario=""
+  senha=""
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form:NgForm){
+    if(!form.valid){
+      form.controls['usuario'].markAsTouched;
+      form.controls['senha'].markAsTouched;
+      if(!form.controls.usuario.valid)
+        this.UsuairioInput?.nativeElement.focus()
+      if(!form.controls.senha.valid)
+        this.SenhaInput?.nativeElement.focus()
+      
+    }  
+    this.login()
+  }
+
+  exibeErro(nomeControle:string,form: NgForm){
+    if(!form.controls[nomeControle])
+      return false
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched
+  }
+
+  login(){
+    // this.usuario
+    // this.senha
+    console.log(this.usuario,this.senha);
+    
+  }
 }
